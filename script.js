23aa0b88784bddbeb924cda4b224385db92af837
egoreq <egoreq@bk.ru>
2020-03-16 13:11:27 +0400

    feat: finished dom

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+//
+//header
+//
 let menuHeader = document.querySelector('.navbar_content');
 
 menuHeader.addEventListener('click',(event)=>{
     menuHeader.querySelectorAll('.header-list_element').forEach(item => item.classList.remove('header-list_element--active'));
     event.target.classList.add('header-list_element--active');
 });
-
+//
 // slider
+//
 let slider = document.querySelector('#slider-show');
 let slides = document.querySelectorAll('.slide');
 let current = 0;
@@ -103,3 +107,64 @@ function screenOff(){
         document.querySelectorAll(".phone_button").forEach(item => item.style.background="black");
     }
 }
+//
+//portfolio
+//
+let tags = document.querySelectorAll('.tag')
+for (let i=0;i<tags.length;i++){
+    tags[i].addEventListener('click', mixPicture);
+}
+function mixPicture() {
+    console.log(tags);
+    let gallery = document.querySelector('.portfolio_pics');
+    let pictures = Array.from(document.querySelectorAll('.portfolio_pics_imgs'));
+    let newGallery = pictures.sort(function() {return Math.random() - 0.5});
+    gallery.innerHTML="";
+    newGallery.forEach(item=>gallery.append(item));
+    tags.forEach(item => item.classList.remove('tag_active'));
+    event.target.classList.add('tag_active');
+}
+
+pictures = document.querySelector('.portfolio_pics');
+
+pictures.addEventListener('click', event=>{
+    if (event.target.tagName==='img'.toUpperCase()){
+        pictures.querySelectorAll('img').forEach(item =>{item.classList.remove('picture_active');});
+        event.target.classList.add('picture_active');
+    }
+});
+//
+//get a qoute
+//
+//form
+
+let form = document.querySelector('#get_a_qoute--form');
+
+document.querySelector('#get_a_qoute--submit').addEventListener('click',  event => {
+    let subject = document.querySelector('#get_a_qoute--subject').value;
+    let describe = document.querySelector('#get_a_qoute--description').value;
+    let them = document.querySelector('#subject-message');
+    let description = document.querySelector('#describe-message');
+    event.preventDefault();
+    if (form.checkValidity()) {
+        if (subject){
+            them.innerHTML ='<i class="message-title">Тема:</i> '+subject;
+        }
+        else {
+            them.innerHTML='<i class="message-title">Без темы</i>';
+        }
+        if (describe){
+            description.innerHTML ='<i class="message-title">Описание:</i> '+describe;
+        }
+        else {
+            description.innerHTML='<i class="message-title">Без описания</i>'; 
+        }
+        document.getElementById('message-block').classList.remove('hidden');
+    }
+    form.reset();
+    return false;
+});
+
+document.querySelector('#button-close').addEventListener('click', event=> {
+    document.querySelector('#message-block').classList.add('hidden');
+});
